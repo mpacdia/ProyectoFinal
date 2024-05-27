5dd59318912658edb1beb833ebbafda2ca62770d
SrCheese44 <sr.cheese44@gmail.com>
2024-05-27 18:31:00 +0100

    outline

--- Assets/Scripts/triggerForOutline.cs
+++ Assets/Scripts/triggerForOutline.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class triggerForOutline : MonoBehaviour
 {
+    public BoxCollider camelArea;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        camelArea = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "cropZone")
     }
 }
