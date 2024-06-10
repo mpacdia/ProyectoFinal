5c98ae9e8c715cc5fd5b32d78ff9b2cdb30369ab
SrCheese45 <mpacdia@gmail.com>
2024-06-10 15:07:13 +0100

    se puede dormir, se cargan las escenas

--- Assets/Scripts/buttonManager.cs
+++ Assets/Scripts/buttonManager.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class buttonManager : MonoBehaviour
 {
+    public GameObject buttons;
+    public GameObject credits;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        buttons.SetActive(true);
+        credits.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void loadNewGame()
+    {
+        SceneManager.LoadScene("mainGame");
+    }
+
+    public void openCredits()
+    {
+        buttons.SetActive(false);
+        credits.SetActive(true);
+    }
+    
+    public void closeCredits()
+    {
+        buttons.SetActive(true);
+        credits.SetActive(false);
     }
 }
---
+++ Assets/Scripts/sleep.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sleep : MonoBehaviour, IInteractable
+{
+    GameObject sun;
+
+    dayNightCycle dayNightCycle;
+    public void Interact()
+    {
+        sun.transform.rotation = Quaternion.identity;
+        dayNightCycle.CurrentDay++;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sun = GameObject.Find("sun");
+        dayNightCycle = sun.GetComponent<dayNightCycle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
