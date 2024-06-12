3fee78a63ab3b0a92d7ca672e2d0d3edb20eb5c8
SrCheese45 <mpacdia@gmail.com>
2024-06-12 16:29:38 +0100

    pesronaje se aliunea con el arbol al talar

--- Assets/Scripts/interactor.cs
+++ Assets/Scripts/interactor.cs
@@ -14,6 +14,12 @@ public class interactor : MonoBehaviour
     public Transform InteractorSource;
     public float InteractorRange;
 
+    public float radius;
+
+    bool isRotating = false;
+
+    RaycastHit hitInfo2;
+
     PlayerInput playerInput;
 
     // Start is called before the first frame update
@@ -25,7 +31,17 @@ public class interactor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isRotating)
+        {
+            Vector3 relativePos = hitInfo2.collider.gameObject.transform.position - transform.position;
 
+            // the second argument, upwards, defaults to Vector3.up
+            Quaternion targetRotation = Quaternion.LookRotation(relativePos, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5.0f);
+            Debug.Log(1 - Mathf.Abs(Quaternion.Dot(transform.rotation, targetRotation)));
+            
+            if (1 - Mathf.Abs(Quaternion.Dot(transform.rotation, targetRotation)) < 0.005f) isRotating = false;
+        }
     }
 
     public void activateInteraction(InputAction.CallbackContext context)
@@ -33,14 +49,29 @@ public class interactor : MonoBehaviour
         if (context.started)
         {
             Ray r = new Ray(InteractorSource.position, InteractorSource.forward);
-            if (Physics.Raycast(r, out RaycastHit hitInfo, InteractorRange))
+            if (Physics.SphereCast(r, radius, out RaycastHit hitInfo, InteractorRange))
             {
+                Debug.Log(hitInfo.collider.gameObject.name);
                 if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
                 {
+                    isRotating = true;
+                    hitInfo2 = hitInfo;
+                    /*Vector3 relativePos = hitInfo.collider.gameObject.transform.position - transform.position;
+
+                    // the second argument, upwards, defaults to Vector3.up
+                    Quaternion targetRotation = Quaternion.LookRotation(relativePos, Vector3.up);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5.0f);
+                    Debug.Log(Time.deltaTime);*/
+                    // INTERPOLACION DE VECTORES / CLAMP/ LERP...
                     interactObj.Interact();
                 }
             }
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawSphere(InteractorSource.position * InteractorRange, radius);
+    }
 }
